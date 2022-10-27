b78499f9e0dd781b138cae6fcf62adb3026876d0
Cobbina Emmanuel <aduboateng333@gmail.com>
2022-10-27 23:24:34 +0000

    First Django Crud

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Artiste, Song, Lyric
 # Register your models here.
+
+admin.site.register(Artiste)
+admin.site.register(Song)
+admin.site.register(Lyric)
--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    age = models.IntegerField()
+
+
+    def __str__(self):
+        return self.first_name
+
+
+class Song(models.Model):
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length=400)
+    date_released = models.DateField()
+    likes = models.IntegerField()
+
+
+    def __str__(self):
+        return self.title
+
+class Lyric(models.Model):
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.CharField(max_length=400)
+
+
+
+    def __str__(self):
+        return self.content
